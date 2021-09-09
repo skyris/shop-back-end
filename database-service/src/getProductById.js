0bf9ead7983e094847d09c5c858467332043a6d6
Victor Klimov <4klimov@gmail.com>
2021-09-09 04:28:16 +0300

    refactor: add 500 error

--- database-service/src/getProductById.js
+++ database-service/src/getProductById.js
@@ -4,13 +4,16 @@ const { notFoundError } = require('./common/errors');
 
 
 async function handler(event) {
-  let product;
+  console.log(event);
   const { productId } = event.pathParameters;
 
-  const client = new Client(dbOptions);
-  await client.connect();
+  let client;
+  let product;
 
   try {
+    client = new Client(dbOptions);
+    await client.connect();
+
     const result = await client.query(`
       SELECT id, title, author, description, count, price
       FROM products
@@ -23,6 +26,7 @@ async function handler(event) {
 
   } catch(err) {
     console.error(`Error during database request execution: ${err}`);
+    return respondJson({message: "Something went wrong"}, 500);
 
   } finally {
     client.end();
--- database-service/src/getProducts.js
+++ database-service/src/getProducts.js
@@ -3,17 +3,21 @@ const { dbOptions, respondJson } = require('./common');
 
 
 async function handler(event) {
+  console.log(event);
+
+  let client;
   let productsList;
 
-  const client = new Client(dbOptions);
-  await client.connect();
-
   try {
+    client = new Client(dbOptions);
+    await client.connect();
+
     const result = await client.query(`SELECT * FROM products;`);
     productsList = result.rows;
 
   } catch(err) {
     console.error(`Error during database request execution: ${err}`);
+    return respondJson({message: "Something went wrong"}, 500);
 
   } finally {
     client.end();
